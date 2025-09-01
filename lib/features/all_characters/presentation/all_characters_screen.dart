082ce308f8f44c4ecf635c47b4011dceed8489eb
GetWet <prokopevmisha766@gmial.com>
2025-09-01 12:55:24 +0300

    All characters screen add functionality

--- lib/features/all_characters/presentation/all_characters_screen.dart
+++ lib/features/all_characters/presentation/all_characters_screen.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:rick_and_morty/widgets/character_card.dart';
 
 class AllCharactersScreen extends StatelessWidget {
   const AllCharactersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("Избранное")),
+      body: ListView.separated(
+        padding: EdgeInsets.all(12),
+        itemBuilder: (context, index) {
+          return CharacterCard();
+        },
+        separatorBuilder: (context, index) {
+          return SizedBox(height: 12);
+        },
+        itemCount: 20,
+      ),
+    );
   }
 }
